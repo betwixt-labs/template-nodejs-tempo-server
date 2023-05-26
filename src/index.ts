dfeee4a2b5ed8805bc02032591950373f8b12a40
andrew <hi@andrew.im>
2023-05-27 01:46:05 +0900

    template: source

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { ConsoleLogger } from '@tempojs/common';
+import { IncomingMessage, ServerResponse, createServer } from 'http';
+import { TempoRouterConfiguration } from '@tempojs/server';
+import { TempoRouter } from '@tempojs/node-http-router';
+import * as Services from './services';
+
+const logger = new ConsoleLogger('Router');
+const config = new TempoRouterConfiguration();
+const registry = new Services.TempoServiceRegistry(logger);
+
+const router = new TempoRouter<object>(logger, registry, config);
+logger.info(`Listening on port 3000`);
+const server = createServer(async (req: IncomingMessage, res: ServerResponse) => {
+	await router.process(req, res, {});
+});
+server.listen(3000);
