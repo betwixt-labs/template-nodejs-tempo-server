36391cc1d12f51e8025d17fa3e235a7750e3d6e0
andrew <hi@andrew.im>
2023-05-27 02:47:53 +0900

    fix: use railway port

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ const config = new TempoRouterConfiguration();
 const registry = new Services.TempoServiceRegistry(logger);
 
 const router = new TempoRouter<object>(logger, registry, config);
-const listenPort = process.env.LISTEN_PORT || 3000; 
+const listenPort = process.env.PORT || 3000; 
 logger.info(`Listening on port: ${listenPort}`);
-process.env.USER_ID; 
 const server = createServer(async (req: IncomingMessage, res: ServerResponse) => {
+	logger.info("Request received");
 	await router.process(req, res, {});
 });
 server.listen(Number(listenPort), "0.0.0.0")
